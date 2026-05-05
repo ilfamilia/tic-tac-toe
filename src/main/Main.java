67aafae519bb79a2cb95fd4edcc0b11ac5e0cd6f
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-05-05 00:36:54 -0400

    Create the game user interfaces

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,13 @@
 package main;
 
+import javax.swing.SwingUtilities;
+import view.GameView;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Is working!!!");
+        SwingUtilities.invokeLater(() -> {
+            GameView gameView = new GameView();
+            gameView.setVisible(true);
+        });
     }
 }
---
+++ src/view/GameView.java
@@ -0,0 +1,65 @@
+package view;
+
+import javax.swing.JFrame;
+import  javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Color;
+
+
+
+public class GameView extends JFrame {
+
+    private JPanel headerPanel;
+    private JPanel mainPanel;
+    private JPanel scorePanel;
+    private JPanel winnerPanel;
+    private JLabel scoreLabel;
+    private JLabel winnerLabel;
+    private JButton[] buttons;
+    private final int BUTTONS_NUM = 9;
+
+    public GameView() {
+        initComponent();
+    }
+
+    public void initComponent() {
+        this.setTitle("Tic-tac-toe");
+        this.setSize(400, 550);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+
+        headerPanel = new JPanel(new BorderLayout());
+
+        mainPanel = new JPanel(new GridLayout(3, 3));
+
+        winnerPanel = new JPanel();
+        scorePanel = new JPanel();
+
+        scoreLabel = new JLabel("<html>Score:<br>X: 0<br>O: 0</html>");
+
+        winnerLabel = new JLabel("X wins!");
+
+        buttons = new JButton[BUTTONS_NUM];
+
+        for(int i = 0; i < 9; i++) {
+            buttons[i] = new JButton();
+        }
+
+        this.add(headerPanel, BorderLayout.NORTH);
+        this.add(this.mainPanel, BorderLayout.CENTER);
+
+        headerPanel.add(winnerPanel, BorderLayout.CENTER);
+        headerPanel.add(scorePanel, BorderLayout.EAST);
+
+        winnerPanel.add(winnerLabel);
+        scorePanel.add(scoreLabel);
+
+        for(JButton button : buttons) {
+            mainPanel.add(button);
+        }
+    }
+}
